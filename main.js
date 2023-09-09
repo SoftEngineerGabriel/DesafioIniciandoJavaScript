9730c1daa895fcc943f766398a8b9267981408c8
SoftEngineerGabriel <farias2752@gmail.com>
2023-09-09 16:11:52 -0300

    Exercicios propostos realizados

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-alert("hello word!")
+/*let variable1 = 2;
+Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+alert("Hello word!")
+
+Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+let variable2 = 3;
+let sum = Number(variable1 + variable2);
+
+console.log(`The sum in between variables ${variable1} + ${variable2} is ${sum}`)
+
+Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+let variable = "12"
+
+if(typeof variable == "number"){
+    alert("The variable is one Number")
+} else{
+    alert("It is not variable")
+}
+//Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+let variable = 12
+
+if(typeof variable == "string"){
+    alert("The variable is one String")
+} else{
+    alert("It is not onde String")
+}
+*/
+//Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+let variable = false;
+
+if(typeof variable === "boolean"){
+    alert("the variavle is one Boolean!")
+} else {
+    alert("it is not one variable!")
+}
